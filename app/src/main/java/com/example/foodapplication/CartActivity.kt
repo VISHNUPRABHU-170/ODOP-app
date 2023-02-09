6d4349bb7e895b81da427f373bb550ff1f866a08
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2023-02-09 12:17:33 +0530

    FOOD app

--- app/src/main/java/com/example/foodapplication/CartActivity.kt
+++ app/src/main/java/com/example/foodapplication/CartActivity.kt
@@ -1,11 +1,25 @@
 package com.example.foodapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        supportActionBar?.hide()
+
+        val in_home =findViewById(R.id.homeicon) as ImageView
+        in_home.setOnClickListener(){
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+        val in_profile=findViewById(R.id.profile) as ImageView
+        in_profile.setOnClickListener(){
+            val intent = Intent(this,ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
