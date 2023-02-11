2d713519cffe3186a1a5d40f74e755b5b490c053
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2023-02-11 23:57:11 +0530

    FOOD app

--- app/src/main/java/com/example/foodapplication/SignupActivity.kt
+++ app/src/main/java/com/example/foodapplication/SignupActivity.kt
@@ -3,8 +3,15 @@ package com.example.foodapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.database.FirebaseDatabase
+import java.util.concurrent.TimeUnit
 
 class SignupActivity : AppCompatActivity() {
 
@@ -12,6 +19,8 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var otp: EditText
     private lateinit var verify: Button
     private lateinit var go: Button
+    private lateinit var resend: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,23 +32,84 @@ class SignupActivity : AppCompatActivity() {
         verify=findViewById(R.id.verify)
         go=findViewById(R.id.go)
 
+        var number=number.text.toString()
+        var otp=otp.text.toString()
+
         verify.setOnClickListener(){
-            val str=number.text.toString()
-            if(str.isBlank()){
-                number.error="Please enter this field"
-            }
-        }
-
-        go.setOnClickListener(){
-            val str=otp.text.toString()
-            if(str.isBlank()){
-                otp.error="Please enter this field"
+            if(number.isBlank() || number.trim().isBlank() || number.length!=10){
+                Toast.makeText(this,"Please enter a valid mobile number ",Toast.LENGTH_SHORT).show()
             }else{
-                val intent=Intent(this,HomeActivity::class.java)
-                startActivity(intent)
+                number = "+91$number"
+
+                val options = PhoneAuthOptions.newBuilder(auth)
+                    .setPhoneNumber(number)       // Phone number to verify
+                    .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                    .setActivity(this)                 // Activity (for callback binding)
+                    .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                    .build()
+                PhoneAuthProvider.verifyPhoneNumber(options)
             }
         }
-
-
     }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+
+
+
+                } else {
+                    // Sign in failed, display a message and update the UI
+
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                Log.d("TAG","Verification failed : ${e.toString()}")
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Log.d("TAG","Verification failed : ${e.toString()}")
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            // Save verification ID and resending token so we can use them later
+
+        }
+    }
+
 }
