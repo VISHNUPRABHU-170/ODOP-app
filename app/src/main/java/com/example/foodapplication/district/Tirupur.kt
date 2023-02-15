af4ad07a0d563c4ec80c8b44148c697ea7d04ea6
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2023-02-15 19:48:54 +0530

    FOOD app

---
+++ app/src/main/java/com/example/foodapplication/district/Tirupur.kt
@@ -0,0 +1,11 @@
+package com.example.foodapplication.district
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tirupur : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_coimbatore)
+    }
+}
